4e570911c7ae54c35f734e10f7271e773220aec3
Kyle McConaughey <kylemcconaughey03@gmail.com>
2020-08-21 00:11:16 -0400

    there is only so long a guy can look at a calculator

--- main.js
+++ main.js
@@ -1,85 +1,41 @@
-let display = document.querySelector(".display")
+const clear = document.querySelector(".clear-btn")
+const display = document.querySelector(".display")
+const uno = document.querySelector('#uno')
+const dos = document.querySelector('#dos')
+const tres = document.querySelector('#tres')
+const quatro = document.querySelector('#quatro')
+const cinco = document.querySelector('#cinco')
+const seis = document.querySelector('#seis')
+const siete = document.querySelector('#siete')
+const ocho = document.querySelector('#ocho')
+const nueve = document.querySelector('#nueve')
+const cero = document.querySelector('#cero')
+const mas = document.querySelector('#mas')
+const menos = document.querySelector('#menos')
+const equals = document.querySelector('#es')
+const izquierda = document.querySelector('#izquierda')
+const derecha = document.querySelector('#derecha')
+const punto = document.querySelector('#punto')
+const poder = document.querySelector('#poder')
+const multi = document.querySelector('#multi')
+const division = document.querySelector('#division')
+const modulo = document.querySelector('#modulo')
 
-let clear = document.querySelector(".clear-btn")
+const buttons = document.querySelectorAll('.btn')
+
+function pushable(which) {
+    for (let each of which) {
+        each.addEventListener('click', function () {
+            display.textContent += each.textContent
+        })
+    }
+}
+
+
+pushable(buttons)
 
 clear.addEventListener("click", function () {
     display.textContent = ""
-})
-
-let one = document.querySelector(".one")
-one.addEventListener("click", function () {
-    display.textContent += "1"
-})
-let two = document.querySelector(".two")
-two.addEventListener("click", function () {
-    display.textContent += "2"
-})
-let three = document.querySelector(".three")
-three.addEventListener("click", function () {
-    display.textContent += "3"
-})
-let four = document.querySelector(".four")
-four.addEventListener("click", function () {
-    display.textContent += "4"
-})
-let five = document.querySelector(".five")
-five.addEventListener("click", function () {
-    display.textContent += "5"
-})
-let six = document.querySelector(".six")
-six.addEventListener("click", function () {
-    display.textContent += "6"
-})
-let seven = document.querySelector(".seven")
-seven.addEventListener("click", function () {
-    display.textContent += "7"
-})
-let eight = document.querySelector(".eight")
-eight.addEventListener("click", function () {
-    display.textContent += "8"
-})
-let nine = document.querySelector(".nine")
-nine.addEventListener("click", function () {
-    display.textContent += "9"
-})
-let zero = document.querySelector(".zero")
-zero.addEventListener("click", function () {
-    display.textContent += "0"
-})
-
-let plus = document.querySelector(".plus")
-plus.addEventListener("click", function () {
-    display.textContent += "+"
-})
-
-let times = document.querySelector(".times")
-times.addEventListener("click", function () {
-    display.textContent += "*"
-})
-
-let minus = document.querySelector(".minus")
-minus.addEventListener("click", function () {
-    display.textContent += "-"
-})
-
-let divide = document.querySelector(".divide")
-divide.addEventListener("click", function () {
-    display.textContent += "/"
-})
-
-let dot = document.querySelector(".dot")
-dot.addEventListener("click", function () {
-    display.textContent += "."
-})
-
-let rightpar = document.querySelector(".rightpar")
-rightpar.addEventListener("click", function () {
-    display.textContent += ")"
-})
-
-let leftpar = document.querySelector(".leftpar")
-leftpar.addEventListener("click", function () {
-    display.textContent += "("
 })
 
 let power = document.querySelector(".power")
@@ -87,12 +43,7 @@ power.addEventListener("click", function () {
     display.textContent += "**"
 })
 
-let modulo = document.querySelector(".modulo")
-modulo.addEventListener("click", function () {
-    display.textContent += "%"
-})
-let equals = document.querySelector(".equals")
-equals.addEventListener("click", function () {
+equals.addEventListener('click', function () {
     let prop = display.innerText
     let answer = eval(prop)
     display.textContent = answer
